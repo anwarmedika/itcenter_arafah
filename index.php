196801a8d053e93fabcbc7ae002b6ce0540ed486
MuhWildanShalahuddin <wildanshalahuddin@gmail.com>
2023-02-13 12:40:19 +0700

    1

---
+++ index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>IT Center - ARAFAH</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="bootstrap-5.1.3/css/bootstrap.css">
+    <style>
+        .card-footer {
+            background-color: navy;
+            color: white;
+            font-weight: bold;
+        }
+
+        img {
+            height: 155px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="row mt-5">
+            <div class="col">
+                <h1 style="text-align: center;font-weight: bold;">IT CENTER ARAFAH</h1>
+            </div>
+        </div>
+        <div class="row mt-3">
+            <div class="col-md-3 col-6 mb-3">
+                <a href="http://arafah.rsanwarmedika.com/ehos_rsam/login">
+                    <div class="card">
+                        <div class="card-body text-center">
+                            <img src="img/ehos.jpg">
+                        </div>
+                        <div class="card-footer">EHOS Arafah</div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-3 col-6 mb-3">
+                <a href="http://arafah.rsanwarmedika.com/simit">
+                    <div class="card">
+                        <div class="card-body text-center">
+                            <img src="img/simit.jpg">
+                        </div>
+                        <div class="card-footer">SIM - IT (Pengaduan Masalah Ke IT)</div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-3 col-6 mb-3">
+                <a href="http://arafaheklaim.rsanwarmedika.com/eklaim/">
+                    <div class="card">
+                        <div class="card-body text-center">
+                            <img src="img/eklaim.jpg">
+                        </div>
+                        <div class="card-footer">Eklaim Arafah</div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-3 col-6 mb-3">
+                <a href="http://arafah.rsanwarmedika.com/surat">
+                    <div class="card">
+                        <div class="card-body text-center">
+                            <img src="img/esurat.jpg">
+                        </div>
+                        <div class="card-footer">E-Surat Arafah</div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-3 col-6 mb-3">
+                <a href="https://sisrute.kemkes.go.id/">
+                    <div class="card">
+                        <div class="card-body text-center">
+                            <img src="img/sisrute.jpg">
+                        </div>
+                        <div class="card-footer">SISRUTE</div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-3 col-6 mb-3">
+                <a href="http://daftarphone.rsanwarmedika.com/">
+                    <div class="card">
+                        <div class="card-body text-center">
+                            <img src="img/phone.jpg">
+                        </div>
+                        <div class="card-footer">Daftar Telepon</div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-3 col-6 mb-3">
+                <a href="http://dinkes.sidoarjokab.go.id/sicantik/public/login?next_url=home">
+                    <div class="card">
+                        <div class="card-body text-center">
+                            <img src="img/sicantik.jpg">
+                        </div>
+                        <div class="card-footer">SI CANTIK</div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-3 col-6 mb-3">
+                <a href="http://sidokararafah.rsanwarmedika.com/sidokararafah">
+                    <div class="card">
+                        <div class="card-body text-center">
+                            <img src="img/sidokar.png">
+                        </div>
+                        <div class="card-footer">SIDOKAR</div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-3 col-6 mb-3">
+                <a href="http://invoicearafah.rsanwarmedika.com/invoicearafah">
+                    <div class="card">
+                        <div class="card-body text-center">
+                            <img src="img/uang.jpg">
+                        </div>
+                        <div class="card-footer">INVOICE ARAFAH</div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+    <script src="bootstrap-5.1.3/js/jquery.custom.js"></script>
+
+</body>
+
+</html>
